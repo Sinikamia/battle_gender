39957ea9c225798ce9c590336b62f8719125a3e8
Sinikamia <nastuska.089601@gmail.com>
2025-04-29 23:27:42 +0300

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:battle_gender/app/router/app_router.dart';
 import 'package:battle_gender/shared/widgets/button/button_main.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   final _appRouter = AppRouter();
   runApp(MyApp(router: _appRouter));
 }
